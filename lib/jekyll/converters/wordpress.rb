e1914262f022878c75b21f6ac626b92f1ff32968
icebreaker <theicebreaker007@gmail.com>
2010-01-31 14:10:29 +0200

    added tag import, fix image URLs so you can just move your uploads folder from wp-content into the images folder and everything should be ok

--- lib/jekyll/converters/wordpress.rb
+++ lib/jekyll/converters/wordpress.rb
@@ -16,8 +16,13 @@ module Jekyll
     # This restriction is made because 'draft' posts are not guaranteed to
     # have valid dates.
     QUERY = "select post_title, post_name, post_date, post_content, post_excerpt, ID, guid from wp_posts where post_status = 'publish' and post_type = 'post'"
+	# Fetch all tags for a given POST ID
+	TAGS_QUERY = "select tm.term_id,tm.name from wp_term_relationships tr 
+				  inner join wp_term_taxonomy tt on tr.term_taxonomy_id = tt.term_taxonomy_id 
+				  inner join wp_terms tm on tm.term_id=tt.term_id  
+				  where tr.object_id=%d and tt.taxonomy = 'post_tag'";
 
-    def self.process(dbname, user, pass, host = 'localhost')
+    def self.process(dbname, user, pass, host = 'localhost', domain = nil)
       db = Sequel.mysql(dbname, :user => user, :password => pass, :host => host)
 
       FileUtils.mkdir_p "_posts"
@@ -31,14 +36,28 @@ module Jekyll
         name = "%02d-%02d-%02d-%s.markdown" % [date.year, date.month, date.day,
                                                slug]
 
+		# Get associated taxonomy terms (tags)
+		# We replace + with nothing and transform to lower case
+		# TODO: figure out what other characters would fuck up YAML
+		tags = []
+		db[TAGS_QUERY % post[:ID]].each do |tag|
+			tags << tag[:name].to_s.gsub('+','').downcase
+		end
+
+		# Process the content and replace URLs pointing to wp_upload
+		# TODO: change this to regexp to be more specific, for the time being
+		# this is OK for my own purposes.
+		if domain
+			content = self.transformUrls(domain,content)
+		end
+
         # Get the relevant fields as a hash, delete empty fields and convert
         # to YAML for the header
         data = {
            'layout' => 'post',
            'title' => title.to_s,
            'excerpt' => post[:post_excerpt].to_s,
-           'wordpress_id' => post[:ID],
-           'wordpress_url' => post[:guid]
+		   'tags' => tags
          }.delete_if { |k,v| v.nil? || v == ''}.to_yaml
 
         # Write out the data and content to file
@@ -50,5 +69,16 @@ module Jekyll
       end
 
     end
+
+	# Process the content and replace URLs pointing to wp_upload
+	# This makes it possible to move the actual "uploads" folder
+	# into your images folder and have all your images working
+	# out of the box automagically ...
+	def self.transformUrls(domain,content)
+		# TODO: add the image class ONLY to links which point to images
+		baseurl = "%s/wp-content/uploads/" % domain
+		return content.gsub(baseurl,"/images/uploads/").gsub('href="/images/uploads/','class="image" href="/images/uploads/')
+	end
+
   end
 end
